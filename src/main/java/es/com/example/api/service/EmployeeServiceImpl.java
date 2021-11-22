1754eea38d8d083f906bd981d3ac3dc7c6df1987
= <=>
2021-11-22 10:24:42 -0600

    Otro commit

--- src/main/java/es/com/example/api/service/EmployeeServiceImpl.java
+++ src/main/java/es/com/example/api/service/EmployeeServiceImpl.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+
 
 import es.com.example.api.dao.EmployeeRepository;
 import es.com.example.api.entity.Employee;
